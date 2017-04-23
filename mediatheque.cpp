78f831e4a5242759f03bf5437a8f14d90c59dbdd
صوفي Sophie <Sophie@MacBook-Pro-de-Sophie.local>
2017-04-23 16:27:04 +0200

    modifs erreur git

--- mediatheque.cpp
+++ mediatheque.cpp
@@ -34,46 +34,11 @@ void mediatheque::affiche(int indice) //Affiche le média d'indice "indice" en d
 
 void mediatheque::ajout(media* const new_media)
 {
-<<<<<<< HEAD
-	m_biblio.push_back(new_media);
-	m_nb_media ++;
-	//new_media.set_id(m_next_entry);		// on donne son index au nouveau media
-	
-	//int new_id = m_biblio.end(); // on recupere l'id que va prendre le media ajoute
-	//m_biblio.pop_back(); 
-
-	// if (m_indices.size() == 0) // si il ne reste aucun indice dispo
-	// {
-	// 	m_liste.push_back(new_media); // on ajoute a la fin
-	// 	(m_liste.back())->set_id(new_id); 
-	// 	m_indices.push_back(m_nb_media); 
-	// }
-	// else
-	// {
-	// 	m_liste[new_id]=new_media;
-	// 	m_liste[new_id]->set_id(new_id);
-	// }
-}
-
-void mediatheque::suppr(int indice)
-=======
 	m_biblio.push_back(new_media); //ajout
-	m_biblio[m_nb_media]->set_id(m_nb_media); //attribution de l'indice
-	m_nb_media ++; 
 }
+
 
 void mediatheque::suppr(int indice)
-{
-	m_biblio.erase(indice); //suppression
-	m_nb_media --; //
-
-	for (int i=indice; i<m_nb_media)
-		m_biblio[i]->set_id(i);
-}
-
-
-void mediatheque::affiche(int indice)
->>>>>>> ec1f5db840a4a5e56479d162aaae594b0ee25460
 {
 	m_biblio.erase(m_biblio.begin() + indice);
 }
--- mediatheque.hpp
+++ mediatheque.hpp
@@ -11,21 +11,7 @@ class mediatheque
 {
 //Attributs
 private :
-<<<<<<< HEAD
 	std::vector<media*> m_biblio; //Liste des médias de la mediatheque
-
-public :
-	int m_nb_media;				// nombre de media dans la mediatheque
-
-	int m_next_entry; 			// indice que prendra le prochain media ajoute
-
-	std::vector<int> m_indices;	// indices que prendront les prochains medias ajoutes
-
-=======
-	std::vector<media*> m_biblio;
-	int m_nb_media;				// nombre de media dans la mediatheque
-
->>>>>>> ec1f5db840a4a5e56479d162aaae594b0ee25460
 
 
 public :
@@ -38,13 +24,9 @@ public :
 	void affiche();
 	void affiche(int indice);
 
-<<<<<<< HEAD
+
 	void ajout(media* const new_media);
 	void suppr(int indice);
-=======
-
-	void suppr(media del_media);
->>>>>>> ec1f5db840a4a5e56479d162aaae594b0ee25460
 
 	// void consulter(int id_cible);
 
