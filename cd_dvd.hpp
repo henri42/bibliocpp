cd5239f7419957fc7346452f1a7f0b0f5b241afc
henri42 <henri.gouttard@gmail.com>
2017-04-24 09:53:49 +0200

    fusion classes cd et dvd, en cd_dvd classe heritee de vhs

---
+++ cd_dvd.hpp
@@ -0,0 +1,17 @@
+/* cd_dvd.hpp */
+#include <string>
+
+class cd_dvd : public vhs
+{
+	//Attributs
+	private :
+
+		int m_piste;					//Nombre de pistes
+	
+	//Méthodes
+	public :
+		// constructeurs et destructeurs
+		cd_dvd();
+		~cd_dvd();
+
+};
