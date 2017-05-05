1650b0999e7891d469690ceeaa50693d6039c4dd
henri42 <henri.gouttard@gmail.com>
2017-05-05 10:04:56 +0200

    UI QT

--- biblioUI/mainwin.cpp
+++ biblioUI/mainwin.cpp
@@ -5,10 +5,18 @@ mainWin::mainWin() : QWidget()
     setFixedSize(800,600);
     setWindowTitle("Bibliozer");
 
-    add = new QPushButton("hide reset",this);
-    del = new QPushButton("show reset",this);
-    add->move(0,0);
-    del->move(add->x()+100,add->y());
+    //construction dynamique des widgets
+    add = new QPushButton("Ajouter",this);
+    del = new QPushButton("Supprimer",this);
+    load = new QPushButton("Charger",this);
+    save = new QPushButton("Sauvegarder",this);
+    list = new QPushButton("Parcourir Médiathèque",this);
+
+    //placement
+    add->move(300,400);
+    del->move(add->x(),add->y() + 30);
+    load->move(add->x()+100,add->y());
+    save->move(del->x()+100,del->y());
 
     username = new QLineEdit(this);
     password = new QLineEdit(this);
